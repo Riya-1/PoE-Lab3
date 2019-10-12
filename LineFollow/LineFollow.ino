4c78b9380e982c435a556e50ce7f721a371c976c
Riya Aggarwal <2000riya@gmail.com>
2019-10-11 20:10:36 -0400

    Final Version w/ Comments

--- LineFollow/LineFollow.ino
+++ LineFollow/LineFollow.ino
@@ -1,17 +1,24 @@
+// Import statements
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
 #include "utility/Adafruit_MS_PWMServoDriver.h"
 
+// Setting up the motors with the Adafruit Motor Shield
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 Adafruit_DCMotor *motorLeft = AFMS.getMotor(1);
 Adafruit_DCMotor *motorRight = AFMS.getMotor(2);
 
+// Pin out of IR Sensors
 int irLeft = A1;
 int irRight = A0;
-int lVal = 700;
-int rVal = 650;
-int mSpeed = 25;
-int mSpeedChange = 100;
+
+// Robot constants, which can be changed via serial
+int lVal = 700;           // Left IR cutoff value
+int rVal = 650;           // Right IR cutoff value
+int mSpeed = 25;          // Motor base speed
+int mSpeedChange = 100;   // Motor added speed
+int rSpeed = mSpeed;      // Setting each motor to base speed
+int lSpeed = mSpeed;
 
 // SERIAL PROTOCOL:
 // Format -> [cmd_char][num_value];
@@ -21,8 +28,12 @@ int mSpeedChange = 100;
 // leading 'r' means set right ir threshold value
 // 's' means start streaming ir values
 // 'd' means stop streaming ir values
+// 'p' means start streaming plotting values
+// 'o' means stop streaming plotting values
 // ';' is terminating character
 
+
+// Serial control variables
 char rx_byte;
 String rx_string = "";
 bool receiving_val = false;
@@ -30,11 +41,11 @@ char last_cmd_received;
 String all_cmds = "balrsdpo";
 bool streaming = false;
 bool plotStreaming = false;
-int rSpeed = mSpeed;
-int lSpeed = mSpeed;
+
 
 
 void setup() {
+  // Start the motors & set up the IR pins
   AFMS.begin();
   motorLeft->setSpeed(rSpeed);
   motorRight->setSpeed(lSpeed);
@@ -47,6 +58,7 @@ void setup() {
 void loop() {
   update_rx();
 
+  // Begin streaming IR Values (for debugging)
   if (streaming) {
     Serial.print("IRSensor  ");
     Serial.print(analogRead(irLeft));
@@ -55,6 +67,7 @@ void loop() {
     Serial.println();
   }
 
+  // Begin streaming IR Values & their corresponding motor values for plotting
   if (plotStreaming) {
     Serial.print(analogRead(irLeft));     Serial.print(",");
     Serial.print(analogRead(irRight));     Serial.print(",");
@@ -62,23 +75,29 @@ void loop() {
     Serial.println(rSpeed);
   }
 
+  // Run the motors
   motorLeft->run(FORWARD);
   motorRight->run(FORWARD);
 
+  // If the Left IR Sensor Value is greater than the threshold
   if (analogRead(irLeft) > lVal)
   {
+    // Increase the right motor speed & set the left motor to zero
     rSpeed = mSpeed + mSpeedChange; lSpeed = 0;
     motorRight->setSpeed(rSpeed);
     motorLeft->setSpeed(lSpeed);
   }
 
+  // If the Right IR Sensor Value is greater than the threshold
   else if (analogRead(irRight) > rVal)
   {
+    // Increase the left motor speed & set the right motor to zero
     lSpeed = mSpeed + mSpeedChange; rSpeed = 0;
     motorLeft->setSpeed(lSpeed);
     motorRight->setSpeed(rSpeed);
   }
 
+  // Otherwise, set both motors to base speed 
   else {
     rSpeed = mSpeed; lSpeed = mSpeed;
     motorRight->setSpeed(mSpeed);
@@ -86,19 +105,26 @@ void loop() {
   }
 }
 
+// Function to handle serial inputs
 void update_rx() {
+
+  // While there character available to read
   while (Serial.available()) {
     rx_byte = Serial.read();
 
+    // Check for a valid command
     if (!receiving_val) {
       if (all_cmds.indexOf(rx_byte) > -1) {
         last_cmd_received = rx_byte;
         rx_string = "";
         receiving_val = true;
       }
-    } else if (rx_byte == ';') {
+    } 
+    // If the command is complete, start processing
+    else if (rx_byte == ';') {
       receiving_val = false;
 
+      // Call the respective method based on the command
       switch (last_cmd_received) {
         case 'b':
           set_base_speed();
@@ -125,12 +151,16 @@ void update_rx() {
           stop_plotting_stream();
           break;
       }
-    } else if (isdigit(rx_byte)) {
+    } 
+
+    // If a number is typed, store it
+    else if (isdigit(rx_byte)) {
       rx_string += rx_byte;
     }
   }
 }
 
+// Set the base speed of the motors
 void set_base_speed() {
   mSpeed = rx_string.toInt();
   Serial.print("Base speed set to ");
@@ -138,6 +168,7 @@ void set_base_speed() {
   return;
 }
 
+// Set the added speed to the motors
 void set_added_speed() {
   mSpeedChange = rx_string.toInt();
   Serial.print("Added speed set to ");
@@ -145,6 +176,7 @@ void set_added_speed() {
   return;
 }
 
+// Set the left IR cutoff value
 void set_left_ir() {
   lVal = rx_string.toInt();
   Serial.print("Left IR threshold value set to ");
@@ -152,6 +184,8 @@ void set_left_ir() {
   return;
 }
 
+
+// Set the right IR cutoff value
 void set_right_ir() {
   rVal = rx_string.toInt();
   Serial.print("Right IR threshold value set to ");
@@ -159,23 +193,30 @@ void set_right_ir() {
   return;
 }
 
+// Start streaming IR values
 void start_streaming() {
   streaming = true;
   Serial.println("Streaming enabled");
   return;
 }
 
+
+// Stop streaming IR values
 void stop_streaming() {
   streaming = false;
   Serial.println("Streaming disabled");
   return;
 }
 
+
+// Start streaming plotting values
 void start_plotting_stream() {
   plotStreaming = true;
   return;
 }
 
+
+// Stop streaming plotting values
 void stop_plotting_stream() {
   plotStreaming = false;
   return;
